1de96afa9dab62e6c792ca8118a54107f0a1b9b1
Ray Chae <hjchae@gmail.com>
2016-12-29 08:57:14 +0900

    Slightly changed...

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,12 +36,8 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
+h = sigmoid(X * theta);
+J = sum( -y .* log(h) - (1-y) .* log(1-h));
 
 
 
