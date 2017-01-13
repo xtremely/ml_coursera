d9cc27b341d3fad6ac7d7fd8af5bf46d34d99c21
xtremely <hjchae@gmail.com>
2017-01-14 04:04:45 +0900

    solved question 4

--- ex3/predict.m
+++ ex3/predict.m
@@ -21,13 +21,17 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = [ones(m, 1) X];   % add bias
 
+z2 = (Theta1 * X')';
+a2 = sigmoid(z2);
 
+a2 = [ones(m, 1) a2]; % add bias to second layer
 
+z3 = (Theta2 * a2')';
+a3 = sigmoid(z3);
 
-
-
-
+[v p] = max(a3, [], 2); % value and class index with max value
 
 % =========================================================================
 
